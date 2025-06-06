afb44cfefd0175fb3379f45de9455b324ba38f8e
redhoodsu <redhoodsu@tencent.com>
2025-06-06 11:47:23 +0800

    feat: use stackblitz for playground

--- docs/.vitepress/theme/components/Playground.vue
+++ docs/.vitepress/theme/components/Playground.vue
@@ -1,34 +1,25 @@
 <script setup>
-import loadJs from 'licia/loadJs'
 import { ref, onMounted } from 'vue'
 import { useData } from 'vitepress'
 
-const { lang } = useData()
-
-const loading = ref(true)
+const { isDark } = useData()
 
 const playground = ref(null)
 
 onMounted(() => {
-  loadJs('//embed.runkit.com', function () {
-    RunKit.createNotebook({
-      element: playground.value,
-      source: [
-        lang.value === 'zh'
-          ? '// 只能引入支持所有环境的模块'
-          : '// Only modules with all platform support can be required.',
-        'const uuid = require("licia/uuid");',
-        'const dateFormat = require("licia/dateFormat");',
-        'const now = require("licia/now");',
-        'const randomBytes = require("licia/randomBytes");\n',
-        'console.log(randomBytes(5));',
-        'console.log(uuid());',
-        'console.log(dateFormat(now(), "yyyy-mm-dd"));',
-      ].join('\n'),
-      onLoad: function () {
-        loading.value = false
-      },
-    })
+  import('@stackblitz/sdk').then((sdk) => {
+    sdk.default.embedProjectId(
+      playground.value,
+      'stackblitz-starters-dlntz4ak',
+      {
+        forceEmbedLayout: true,
+        openFile: 'index.js',
+        theme: isDark.value ? 'dark' : 'light',
+        hideExplorer: true,
+        view: 'editor',
+        terminalHeight: 50,
+      }
+    )
   })
 })
 </script>
@@ -36,7 +27,7 @@ onMounted(() => {
 <template lang="pug">
 .container.mobile
   .playground(ref="playground")
-    .initial-text(v-if="loading") {{ lang === 'zh' ? '初始化 RunKit 中...' : 'Initializing RunKit...' }}
+    .initial-text(v-if="loading") {{ lang === 'zh' ? '初始化 StackBlitz 中...' : 'Initializing StackBlitz...' }}
 </template>
 o
 <style lang="stylus">
@@ -52,4 +43,9 @@ o
         top 0
         left 0
         text-align center
+iframe.playground
+    width 100%
+    min-height 600px
+    padding-top 0
+    border 1px solid var(--vp-c-gutter)
 </style>
